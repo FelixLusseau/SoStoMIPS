bad24c667ee352d268762ab75dc1f1ed98a38642
louise.couture@etu.unistra.fr <louise.couture@etu.unistra.fr>
2022-11-22 18:22:14 +0100

    quads

--- yacc/quads.h
+++ yacc/quads.h
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* opérande d'une instruction à 3 addresse: entier/variable/addresse goto*/
 typedef struct quadOP {
-    enum{QO_CST=1,QO_NAME}kind;
+    enum{QO_CST=1,QO_NAME,QO_ADDR}kind;
     union{int cst;char *name;}u;
 } quadOP;
+
 quadOP  QOcreat_cst(int v);
 quadOP  QOcreat_name(char* v);
 
+/* quad / instruction à 3 addresse*/
 typedef struct quads {
     enum{Q_ADD=100,Q_MUL,Q_ASS,Q_GOTO}kind;
     quadOP op1,op2,res;
@@ -15,7 +18,7 @@ typedef struct quads {
 } quads;
 quads * Qcreat(int type,quadOP op1, quadOP op2, quadOP res);
 
-
+/* liste stockant les quads */
 typedef struct listQ {
     struct quads *first;
     int taille;
